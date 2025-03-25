83e6922a9b5d7bde9f84b5df9e394286bdc5f3c0
Shivam Tanwar <shivamtanwar0801@gmail.com>
2025-03-25 19:12:21 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,7 @@ import PlacePage from "./pages/PlacePage";
 import BookingsPage from "./pages/BookingsPage";
 import BookingPage from "./pages/BookingPage";
 
-axios.defaults.baseURL = "http://localhost:4000/api/v1";
+axios.defaults.baseURL = "https://airbnb-clone-shivam-tanwar.onrender.com/api/v1";
 axios.defaults.withCredentials = true;
 
 function App() {
